725dfdac027ee8261a31ed7e0519d1d16789e7b1
tajbirahmed <tajbir26@gmail.com>
2025-02-22 23:27:32 +0600

    feat: added user model, user schma creation sql, bcryptpasswordencoder, userentity

--- user-service/src/main/java/com/bytebandit/userservice/model/UserEntity.java
+++ user-service/src/main/java/com/bytebandit/userservice/model/UserEntity.java
@@ -1,10 +1,14 @@
 package com.bytebandit.userservice.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -23,17 +27,22 @@ public class UserEntity {
     private UUID id;
 
     @Column(name = "email", unique = true)
+    @Email(message = "INVALID EMAIL FORMAT DETECTED")
+    @NotNull(message = "EMAIL FIELD CANNOT BE NULL")
     private String email;
 
-    @Column(name = "password")
+    @Column(name = "password", length = 60)
+    @NotNull(message = "PASSWORD FIELD CANNOT BE NULL")
     private String password;
 
     @Column(name = "oauth_id")
     private String oauthId;
 
-    @Column(name = "created_at")
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
     private Timestamp createdAt;
 
     @Column(name = "updated_at")
+    @UpdateTimestamp
     private Timestamp updatedAt;
 }
--- user-service/src/main/resources/schema.sql
+++ user-service/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users
 (
     id         UUID NOT NULL,
-    email      VARCHAR(255),
+    email      VARCHAR(255) NOT NULL,
     password   VARCHAR(255),
     oauth_id   VARCHAR(255),
     created_at TIMESTAMP WITHOUT TIME ZONE,
