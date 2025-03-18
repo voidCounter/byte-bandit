94cca456c11b704dd677a81914d8bc84091b7d0c
tajbirahmed <tajbir26@gmail.com>
2025-03-18 22:33:32 +0600

    made EmailTemplateConfig (formerly ThymeleafConfig) more cleaner

--- apps/user-service/src/main/java/com/bytebandit/userservice/config/EmailTemplateConfig.java
+++ apps/user-service/src/main/java/com/bytebandit/userservice/config/EmailTemplateConfig.java
@@ -5,36 +5,43 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
+import java.nio.charset.StandardCharsets;
+
 @Configuration
-public class ThymeleafConfig {
+public class EmailTemplateConfig {
 
     @Bean
     public ITemplateResolver emailTemplateResolver() {
         ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setPrefix("registration-mail/");
-        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setPrefix("templates/emails/");
         templateResolver.setSuffix(".html");
-        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        templateResolver.setOrder(1);
+        templateResolver.setCheckExistence(true);
         return templateResolver;
     }
 
     @Bean
-    public ResourceBundleMessageSource emailMessageSource() {
-        final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("localization-registration/messages");
-        return messageSource;
-    }
-
-    @Bean
-    public SpringTemplateEngine thymeleafEmailTemplateEngine(
+    public SpringTemplateEngine emailTemplateEngine(
             @Qualifier("emailTemplateResolver") ITemplateResolver templateResolver
     ) {
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver);
         templateEngine.setTemplateEngineMessageSource(emailMessageSource());
         return templateEngine;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource emailMessageSource() {
+        final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames(
+                "templates/localizations/registration/messages"
+        );
+        return messageSource;
     }
 }
