ded77f89e2465656f99edd76072a9ce5fc98bee9
tajbirahmed <tajbir26@gmail.com>
2025-03-19 16:03:56 +0600

    added failure test of email with smtp port error

--- apps/user-service/src/test/java/com/bytebandit/userservice/service/RegistrationEmailServiceIntegrationTest.java
+++ apps/user-service/src/test/java/com/bytebandit/userservice/service/RegistrationEmailServiceIntegrationTest.java
@@ -1,13 +1,16 @@
 package com.bytebandit.userservice.service;
 
 import com.bytebandit.userservice.enums.EmailTemplate;
+import com.bytebandit.userservice.exception.ErrorSendingEmailException;
 import com.icegreen.greenmail.configuration.GreenMailConfiguration;
 import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.GreenMailUtil;
 import com.icegreen.greenmail.util.ServerSetupTest;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
@@ -15,6 +18,7 @@ import org.thymeleaf.spring6.SpringTemplateEngine;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
@@ -59,6 +63,28 @@ class RegistrationEmailServiceIntegrationTest {
         MimeMessage receivedMessage = receivedMessages[0];
         assertThat(receivedMessage.getAllRecipients()[0]).hasToString(recipient);
         assertThat(receivedMessage.getSubject()).isEqualTo(subject);
+        assertThat(GreenMailUtil.getBody(receivedMessage)).contains(body);
         verify(templateEngine).process(eq("template.html"), any());
+    }
+
+    @Test
+    void sendEmail_ShouldHandleSmtpError() {
+        String recipient = "user@example.com";
+        String subject = "Welcome!";
+        EmailTemplate emailTemplate = mock(EmailTemplate.class);
+        Map<String, Object> templateModel = Map.of("name", "John");
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("non-existent-host");
+        mailSender.setPort(9999);
+
+        SpringTemplateEngine templateEngine = mock(SpringTemplateEngine.class);
+        when(templateEngine.process(eq(emailTemplate.getTemplatePath()), any())).thenReturn("body");
+
+        RegistrationEmailService registrationEmailService = new RegistrationEmailService(templateEngine, mailSender);
+
+        assertThatThrownBy(() ->
+                registrationEmailService.sendEmail(recipient, subject, emailTemplate, templateModel))
+                .isInstanceOf(MailSendException.class);
     }
 }
