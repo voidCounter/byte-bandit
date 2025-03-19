a24dc6736c388ec0c755f937062d80402a8f57df
tajbirahmed <tajbir26@gmail.com>
2025-03-19 15:22:21 +0600

    added tests for registration email service using greenemail

---
+++ apps/user-service/src/test/java/com/bytebandit/userservice/service/RegistrationEmailServiceIntegrationTest.java
@@ -0,0 +1,60 @@
+package com.bytebandit.userservice.service;
+
+import com.bytebandit.userservice.enums.EmailTemplate;
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+
+class RegistrationEmailServiceIntegrationTest {
+
+    @RegisterExtension
+    static GreenMailExtension greenMailExtension = new GreenMailExtension(ServerSetupTest.SMTP_IMAP)
+            .withConfiguration(GreenMailConfiguration
+                    .aConfig()
+                    .withUser("foo@localhost", "foo", "foo-pwd"))
+                    .withPerMethodLifecycle(false);
+
+    @Test
+    void sendEmail_ShouldActuallySendEmailViaSmtp() throws MessagingException {
+        String recipient = "user@example.com";
+        String subject = "Welcome!";
+        String body = "Welcome to our platform!";
+        EmailTemplate emailTemplate = mock(EmailTemplate.class);
+        when(emailTemplate.getTemplatePath()).thenReturn("template.html");
+
+        Map<String, Object> templateModel = Map.of("name", "John");
+        SpringTemplateEngine templateEngine = mock(SpringTemplateEngine.class);
+        when(templateEngine.process(eq("template.html"), any())).thenReturn(body);
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("localhost");
+        mailSender.setPort(ServerSetupTest.SMTP.getPort()); // GreenMail SMTP port
+        mailSender.setUsername("");
+        mailSender.setPassword("");
+
+        RegistrationEmailService registrationEmailService = new RegistrationEmailService(templateEngine, mailSender);
+        registrationEmailService.sendEmail(recipient, subject, emailTemplate, templateModel);
+        greenMailExtension.waitForIncomingEmail(1);
+        MimeMessage[] receivedMessages = greenMailExtension.getReceivedMessages();
+
+        assertThat(receivedMessages).hasSize(1);
+        MimeMessage receivedMessage = receivedMessages[0];
+        assertThat(receivedMessage.getAllRecipients()[0]).hasToString(recipient);
+        assertThat(receivedMessage.getSubject()).isEqualTo(subject);
+        verify(templateEngine).process(eq("template.html"), any());
+    }
+}
---
+++ apps/user-service/src/test/java/com/bytebandit/userservice/service/RegistrationEmailServiceTest.java
@@ -0,0 +1,48 @@
+package com.bytebandit.userservice.service;
+
+import com.bytebandit.userservice.enums.EmailTemplate;
+import jakarta.mail.Session;
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class RegistrationEmailServiceTest {
+
+    @Mock
+    private SpringTemplateEngine springTemplateEngine;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Spy
+    @InjectMocks
+    private RegistrationEmailService registrationEmailServiceSpy;
+
+    @Test
+    void sendEmail_ShouldProcessTemplateAndSendEmail() {
+        String sendEmailTo = "user@example.com";
+        String emailSubject = "Welcome!";
+        String emailBody = "<html>Email Content</html>";
+        EmailTemplate emailTemplate = mock(EmailTemplate.class);
+        when(emailTemplate.getTemplatePath()).thenReturn("template.html");
+        Map<String, Object> templateModel = Map.of("name", "John");
+        when(springTemplateEngine.process(eq("template.html"), any(Context.class))).thenReturn(emailBody);
+        MimeMessage mimeMessage = new MimeMessage((Session) null);
+        when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+        registrationEmailServiceSpy.sendEmail(sendEmailTo, emailSubject, emailTemplate, templateModel);
+        verify(springTemplateEngine).process(eq("template.html"), any(Context.class));
+        verify(registrationEmailServiceSpy).sendMailMessage(javaMailSender, sendEmailTo, emailSubject, emailBody);
+    }
+
+}
