bb06723e6e5990c432de7912d302b585f15571d4
voidCounter <rohitroy7014@gmail.com>
2025-04-05 19:44:21 +0600

    chore: added develop's updates, add missing @ActiveProfiles annotations

--- apps/backend/user-service/src/test/java/com/bytebandit/userservice/entity/TokenEntityTest.java
+++ apps/backend/user-service/src/test/java/com/bytebandit/userservice/entity/TokenEntityTest.java
@@ -17,10 +17,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class TokenEntityTest {
 
     @Autowired
