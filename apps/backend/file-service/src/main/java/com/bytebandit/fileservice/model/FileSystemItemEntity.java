17cb1f385df589e7c6b719255a1b7a24de84b63b
tajbirahmed <tajbir26@gmail.com>
2025-04-21 15:50:32 +0600

    fix: fixed fetch type

--- apps/backend/file-service/src/main/java/com/bytebandit/fileservice/model/FileSystemItemEntity.java
+++ apps/backend/file-service/src/main/java/com/bytebandit/fileservice/model/FileSystemItemEntity.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -83,6 +84,7 @@ public class FileSystemItemEntity {
 
     @OneToMany(
         mappedBy = "item",
+        fetch = FetchType.LAZY,
         cascade = CascadeType.ALL,
         orphanRemoval = true
     )
@@ -90,7 +92,6 @@ public class FileSystemItemEntity {
 
     @OneToOne(
         mappedBy = "item",
-        cascade = CascadeType.ALL,
         orphanRemoval = true
     )
     private SharedItemsPublicEntity sharedItem;
