ba5fe317f2fc3b5a15cbfcc0747effb1fc46a06a
tajbirahmed <tajbir26@gmail.com>
2025-04-28 12:41:59 +0600

    fix: reafactore RoleBasedAccessControl tests

--- apps/backend/file-service/src/test/java/com/bytebandit/fileservice/configurer/AbstractPostgresContainer.java
+++ apps/backend/file-service/src/test/java/com/bytebandit/fileservice/configurer/AbstractPostgresContainer.java
@@ -38,7 +38,6 @@ public abstract class AbstractPostgresContainer {
         registry.add("spring.datasource.driver-class-name",
             POSTGRESQL_CONTAINER::getDriverClassName
         );
-        registry.add("spring.jpa.defer-datasource-initialization", () -> true);
-        registry.add("spring.sql.init.mode", () -> "always");
+        registry.add("spring.jpa.defer-datasource-initialization", () -> false);
     }
 }
