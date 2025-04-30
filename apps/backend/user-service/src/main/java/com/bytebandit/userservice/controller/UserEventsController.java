6774000c36fafbe5a12f343df7be622631c49ec1
tajbirahmed <tajbir26@gmail.com>
2025-04-30 13:18:41 +0600

    feat: added api documentation in user-service

--- apps/backend/user-service/src/main/java/com/bytebandit/userservice/controller/UserEventsController.java
+++ apps/backend/user-service/src/main/java/com/bytebandit/userservice/controller/UserEventsController.java
@@ -1,6 +1,8 @@
 package com.bytebandit.userservice.controller;
 
 import com.bytebandit.userservice.service.UserEventProducer;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lib.core.dto.response.ApiResponse;
 import lib.core.events.UserEvent;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/events")
+@Tag(
+    name = "User event that is reponsible for sending user events to Kafka"
+    + "which in turn inserts the data into snapshot table of the file service"
+    + "or wherever a user snapshot is needed",
+    description = "APIs for user events"
+)
 public class UserEventsController {
     private final UserEventProducer userEventProducer;
     
@@ -24,6 +32,10 @@ public class UserEventsController {
      *
      * @return ApiResponse indicating the success of the operation.
      */
+    @Operation(
+        summary = "Create user event",
+        description = "Creates a user event and sends it to Kafka."
+    )
     @PostMapping("/create")
     public ApiResponse<Boolean> createUserEvent(@RequestBody UserEvent userEvent) {
         userEventProducer.sendUserEvent(userEvent);
--- apps/backend/user-service/src/main/java/com/bytebandit/userservice/dto/ResendVerificationRequest.java
+++ apps/backend/user-service/src/main/java/com/bytebandit/userservice/dto/ResendVerificationRequest.java
@@ -1,5 +1,6 @@
 package com.bytebandit.userservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -7,11 +8,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "Request to resend email verification containing email address.")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResendVerificationRequest {
+
+    @Schema(
+        description = "The email address of the user for verification."
+    )
     @NotBlank(message = "Email cannot be empty")
     @Email(message = "Invalid email format")
     String email;
