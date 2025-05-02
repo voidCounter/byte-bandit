6020f5cfd8535efafc59e9c9c0d757d45f14f61b
tajbirahmed <tajbir26@gmail.com>
2025-05-02 22:50:33 +0600

    feat: added item move service

--- apps/backend/file-service/src/main/java/com/bytebandit/fileservice/controller/UpdateItemController.java
+++ apps/backend/file-service/src/main/java/com/bytebandit/fileservice/controller/UpdateItemController.java
@@ -1,5 +1,6 @@
 package com.bytebandit.fileservice.controller;
 
+import com.bytebandit.fileservice.dto.MoveItemRequest;
 import com.bytebandit.fileservice.dto.UpdateItemRequest;
 import com.bytebandit.fileservice.service.UpdateItemService;
 import com.bytebandit.fileservice.utils.HttpHeaderUtils;
@@ -22,7 +23,15 @@ public class UpdateItemController {
     public UpdateItemController(UpdateItemService updateItemService) {
         this.updateItemService = updateItemService;
     }
-    
+
+    /**
+     * Handles the request to rename an item.
+     *
+     * @param request the request containing the item ID and new name
+     * @param servletRequest the HTTP servlet request
+     *
+     * @return a response entity containing the result of the rename operation
+     */
     @PostMapping("/rename")
     public ResponseEntity<ApiResponse<String>> renameItem(
         @Valid @RequestBody UpdateItemRequest request,
@@ -37,6 +46,32 @@ public class UpdateItemController {
                 .data(updateItemService.updateItem(request, userId))
                 .timestamp(String.valueOf(System.currentTimeMillis()))
                 .path("/update/rename")
+                .build()
+        );
+    }
+
+    /**
+     * Handles the request to move an item.
+     *
+     * @param request the request containing the item ID and new location
+     * @param servletRequest the HTTP servlet request
+     *
+     * @return a response entity containing the result of the move operation
+     */
+    @PostMapping("/move")
+    public ResponseEntity<ApiResponse<String>> moveItem(
+        @Valid @RequestBody MoveItemRequest request,
+        @NotNull HttpServletRequest servletRequest
+    ) {
+        final String userId = HttpHeaderUtils.getUserIdHeader(servletRequest);
+
+        return ResponseEntity.ok(
+            ApiResponse.<String>builder()
+                .status(200)
+                .message("Moved item successfully")
+                .data(updateItemService.moveItem(request, userId))
+                .timestamp(String.valueOf(System.currentTimeMillis()))
+                .path("/update/move")
                 .build()
         );
     }
